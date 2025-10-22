32df32eb9215a0bf768151d698fc9c482f9fc516
Ilabarry <barryila20@gmail.com>
2025-10-22 09:14:14 +0000

    Scroll indicator

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -249,7 +249,7 @@ const Hero = () => {
 
       {/* Scroll indicator */}
       <motion.div
-        className="absolute bottom-12 left-1/2 transform -translate-x-1/2"
+        className="absolute bottom-10 left-1/2 transform -translate-x-1/2"
         animate={{ y: [0, 10, 0] }}
         transition={{ duration: 2, repeat: Infinity }}
       >
