d5ed83d7f0d03a2aa930ce21003b8dff89ce9216
Ilabarry <barryila20@gmail.com>
2025-10-22 09:08:32 +0000

    changement de padding

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -53,7 +53,7 @@ const Hero = () => {
         <div className="absolute bottom-1/4 right-1/4 w-96 h-96 bg-purple-500 rounded-full blur-3xl"></div>
       </motion.div>
 
-      <div className="container mx-auto px-4 py-16 relative z-10">
+      <div className="container mx-auto px-4 py-14 relative z-10">
         <div className="flex flex-col lg:flex-row items-center justify-between gap-12">
           
           {/* Colonne texte animée */}
